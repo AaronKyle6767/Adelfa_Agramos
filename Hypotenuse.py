6404f360d85b8cc86f69810eaf2fb7f265745f4b
AaronKyle6767 <acagramos@carc.pshs.edu.ph>
2026-09-24 16:14:19 +0800

    Update input prompts and output message for clarity

--- Hypotenuse.py
+++ Hypotenuse.py
@@ -1,9 +1,12 @@
 #Imported math is required to solve for the hypotenuse
 import math
+
 #Inputs a and b
-a = float(input("Enter a number for a: "))
-b = float(input("Enter a number for b: "))
+a = float(input("Enter a number for side a: "))
+b = float(input("Enter a number for side b: "))
+
 #Main computation
 c = math.sqrt(pow(a,2)+pow(b,2))
 #Final Output
-print(f"The hypotenuse is: {c:.2f}")
+
+print(f"The hypotenuse of sides {a} and {b} is: {c:.2f}")
